67020bc1cf60c4db68805a8d83adeb9d147e775e
… <vsugare1@educacion.nnavarra.es>
2026-08-19 23:33:42 +0200

    Refactor game state and loop into Game class

---
+++ src/core/game.ts
@@ -0,0 +1,48 @@
+import { createEnemy, Enemy } from "../entities/enemy.js";
+import { Grid, TileType } from "../grid.js";
+import { render as renderGrid, renderEnemy } from "../render.js";
+
+export class Game{
+
+    grid: Grid
+    enemy: Enemy 
+    lastTimeStamp: number = 0
+    ctx: CanvasRenderingContext2D
+
+    constructor( ctx: CanvasRenderingContext2D){
+        this.ctx = ctx;
+        this.grid = new Grid({columns: 12, rows: 9});
+
+        const pathRow = 4;
+        for (let col = 0; col < this.grid.columns; col++) {
+          this.grid.setTile(col, pathRow, TileType.Path);
+        }
+
+        this.enemy = createEnemy();
+        this.enemy.velocity.x = 50;
+    }
+
+    update(deltaTime: number): void{
+          this.enemy.position.x += this.enemy.velocity.x * deltaTime
+          this.enemy.position.y += this.enemy.velocity.y * deltaTime
+          console.log(this.enemy.position) 
+    }
+
+    render(){
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+        renderGrid(this.grid, this.ctx);
+        renderEnemy(this.enemy, this.ctx)
+    }
+
+    start(){
+        const tick = (timestamp: number) => {
+            let deltaTime = (timestamp - this.lastTimeStamp)/1000;
+            this.lastTimeStamp = timestamp;
+            this.update(deltaTime);
+            this.render();
+            requestAnimationFrame(tick)
+        };
+
+        requestAnimationFrame(tick)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
+import { Game } from "./core/game.js";
 import { createEnemy, Enemy } from "./entities/enemy.js";
 import { Grid, TileType, TILE_SIZE } from "./grid.js";
 import { render, renderEnemy } from "./render.js";
-
-const grid = new Grid({ columns: 12, rows: 9 });
-
-const pathRow = 4;
-for (let col = 0; col < grid.columns; col++) {
-  grid.setTile(col, pathRow, TileType.Path);
-}
-
 
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 if (canvas == null) {
@@ -19,28 +12,6 @@ const ctx = canvas.getContext("2d");
 if (ctx == null) {
   throw new Error("Context is null");
 }
-  let lastTimestamp = 0;
 
-  const enemy:Enemy = createEnemy();
-  enemy.velocity.x = 10;
-  enemy.velocity.y = 10;
-
-
-render(grid, ctx);
-
-  function loop(timestamp: number, ctx: CanvasRenderingContext2D){
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  let deltaTime: number = (timestamp - lastTimestamp)/1000;
-  lastTimestamp = timestamp;
-
-
-  enemy.position.x += enemy.velocity.x * deltaTime
-  enemy.position.y += enemy.velocity.y * deltaTime
-  console.log(enemy.position) 
-  render(grid, ctx)
-  renderEnemy(enemy, ctx)
-
-  requestAnimationFrame((ts) => loop(ts,ctx));
-}
-
-requestAnimationFrame((ts) => loop(ts,ctx));
+const game = new Game(ctx);
+game.start;
